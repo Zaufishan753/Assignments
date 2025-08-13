8bc0f15eb549190bd2078946bb3ee8694d9171a9
Zaufishan753 <zaufishanansari3556@gmail.com>
2025-08-13 23:35:54 +0530

    Add files via upload

---
+++ swapping.py
@@ -0,0 +1,11 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = int(input("Enter first number"))
+Enter first number10
+>>> b = int(input("Enter second number: "))
+Enter second number: 15
+>>> a,b=b,a
+>>> print("after swapping first number",a)
+after swapping first number 15
+>>> print("after swapping second number",b)
+after swapping second number 10
